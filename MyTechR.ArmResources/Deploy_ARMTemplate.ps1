481880c4279968569785fb6eccf496d2b9a06f25
dick bevernage <dick.bevernage@ae.be>
2017-05-02 16:03:33 +0200

    Initial Azure ARM config for linux ubuntu VM

---
+++ MyTechR.ArmResources/Deploy_ARMTemplate.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("TechRadarRsxGroup")]
+    $resourceGroupName
+    )
+
+Login-AzureRmAccount
+New-AzureRmResourceGroup -Name $resourceGroupName -Location "West Europe"
+New-AzureRmResourceGroupDeployment -Name myTechRdrDeploy -ResourceGroupName $resourceGroupName -TemplateFile .\AzureDeploy.json
+
+#Remove-AzureRmResourceGroup -Name $resourceGroupName
