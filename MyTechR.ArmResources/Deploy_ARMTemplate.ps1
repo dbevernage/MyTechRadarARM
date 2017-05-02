703836ed0013e7381c89c3b3ce35195ea14dd5e8
dick bevernage <dick.bevernage@ae.be>
2017-05-02 17:54:35 +0200

    Split out ARM resources in separate files

--- MyTechR.ArmResources/Deploy_ARMTemplate.ps1
+++ MyTechR.ArmResources/Deploy_ARMTemplate.ps1
@@ -4,8 +4,10 @@
     $resourceGroupName
     )
 
+$rsxGroupDeployment = "myTechRdrDeploy"
 Login-AzureRmAccount
-New-AzureRmResourceGroup -Name $resourceGroupName -Location "West Europe"
-New-AzureRmResourceGroupDeployment -Name myTechRdrDeploy -ResourceGroupName $resourceGroupName -TemplateFile .\AzureDeploy.json
+#New-AzureRmResourceGroup -Name $resourceGroupName -Location "West Europe"
+#New-AzureRmResourceGroupDeployment -Name $rsxGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\AzureDeploy.json
+New-AzureRmResourceGroupDeployment -Name $rsxGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\AzureDeploy_ContainerRegistry.json
 
 #Remove-AzureRmResourceGroup -Name $resourceGroupName
